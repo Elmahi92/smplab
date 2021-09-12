35fa68be913c0ce43b55d7a9ab7033c8375898d6
Mohamed Ali <mohamedelmahi92@outlook.com>
2021-09-13 00:03:39 +0200

    Adding desc

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,3 +1,13 @@
+#' Dijkstra's algorithm
+#'
+#' @param graph A data frame that contains the edges of the graph and the weight of the edge
+#' @param init_node A numeric scalar that exists in the graph
+#' @return The shortest distance from \code{init_node} to every other node
+#' @examples
+#' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6), v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5), w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 1)
+#' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6), v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5), w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 3)
+#' @references \url{https://en.wikipedia.org/wiki/Dijkstras_algorithm/}
+#' @export
 dijkstra <- function(graph, init_node) {
   nodes <- unique(graph[,1])
   dist <- rep(Inf, length(nodes))
